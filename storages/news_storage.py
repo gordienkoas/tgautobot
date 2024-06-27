10e387c8b6217eae0de8a14cc3e6fdfd52e742b7
gordienkoas <gordienkoas@bk.ru>
2024-06-27 09:00:27 +0300

    make storage

---
+++ storages/news_storage.py
@@ -0,0 +1,31 @@
+from typing import Hashable
+
+from storages.base_storage import BaseStorage
+
+class NewsStorage(BaseStorage):
+    STORAGE_FILE_NAME = 'news'
+
+    def store_element(self, element: Hashable):
+        data = self.get_data()
+        if not data:
+            data = dict()
+
+        data[element] = element
+        self.save_data(data)
+
+
+if __name__ == '__main__':
+    from news_sources.types import News
+
+    storage = NewsStorage()
+    storage.store_element(News(
+        title='title',
+        summary='summary',
+        img_url=''
+    ))
+    storage.store_element(News(
+        title='title',
+        summary='summary',
+        img_url=''
+    ))
+    print(storage.get_data())
